f7db4efbdc425ae4a214bc4d6fda07b89b9cf479
Marina <marina.lsz522@gmail.com>
2024-08-28 13:14:02 +0200

    Initial commit

---
+++ ejercicio4/script.js
@@ -0,0 +1,17 @@
+/* 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
+evento click que ejecute un console log con la información del evento del click
+
+1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
+
+1.3 Añade un evento 'input' que ejecute un console.log con el valor del input. */
+
+/* Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html 
+que imprima cada uno de los albums.
+
+const albums = [
+  "De Mysteriis Dom Sathanas",
+  "Reign of Blood",
+  "Ride the Lightning",
+  "Painkiller",
+  "Iron Fist",
+]; */
