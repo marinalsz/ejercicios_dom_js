e8d0b89f831b9bf7d1e6b4e6d41db7a0ce134378
Marina <marina.lsz522@gmail.com>
2024-09-10 18:41:47 +0200

    Modificados ejercicios

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -1,14 +1,29 @@
-//1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+//1.1
 
-//1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+const button = document.querySelector(".showme");
+console.log(button);
 
-//1.3 Usa querySelector para mostrar por consola todos los p
+//1.2
 
-//1.4 Usa querySelector para mostrar por consola todos los elementos con 
-//la clase.pokemon
+const title = document.querySelector("#pillado");
+console.log(title);
 
-//1.5 Usa querySelector para mostrar por consola todos los elementos con 
-//el atributo data-function="testMe".
+//1.3
 
-//1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
-//data-function="testMe".
+const p = document.querySelectorAll("p");
+console.log(p);
+
+//1.4
+
+const pokemon = document.querySelectorAll(".pokemon");
+console.log(pokemon);
+
+//1.5
+
+const testMe = document.querySelectorAll('[data-function="testMe"]');
+console.log(testMe);
+
+//1.6
+
+const testMeThird = document.querySelectorAll('[data-function="testMe"]');
+console.log(testMeThird[2]);
