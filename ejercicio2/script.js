c1099ae1ebcd51e0d952ae5051e0be865c9ff113
Marina <marina.lsz522@gmail.com>
2024-09-27 13:42:42 +0200

    Modificado ejercicio2

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -1,14 +1,17 @@
 //2.1
+
 const nuevoDiv = document.createElement("div");
 document.body.appendChild(nuevoDiv);
 
 //2.2
+
 const newDiv = document.createElement("div");
 const nuevoParrafo = document.createElement("p");
 newDiv.appendChild(nuevoParrafo);
 document.body.appendChild(newDiv);
 
 //2.3
+
 const newDivisor = document.createElement("div");
 for (let i = 0; i < 6; i++) {
   const nuevoP = document.createElement("p");
@@ -17,15 +20,18 @@ for (let i = 0; i < 6; i++) {
 document.body.appendChild(newDivisor);
 
 //2.4
+
 const p = document.createElement("p");
 p.textContent = "Soy dinámico!";
 document.body.appendChild(p);
 
 //2.5
+
 const h2 = document.querySelector(".fn-insert-here");
 h2.textContent = "Wubba Lubba dub dub";
 
 //2.6
+
 const apps = ["Facebook", "Netflix", "Instagram", "Snapchat", "Twitter"];
 const ul = document.createElement("ul");
 for (const app of apps) {
@@ -36,18 +42,21 @@ for (const app of apps) {
 document.body.appendChild(ul);
 
 //2.7
+
 const removedElements = document.querySelectorAll(".fn-remove-me");
 for (const element of removedElements) {
   element.remove();
 }
 
 //2.8
+
 const newP = document.createElement("p");
 newP.textContent = "Voy en medio!";
-const divisor = document.querySelector("div.fn-insert-here");
+const divisor = document.querySelector("div");
 divisor.insertAdjacentElement("afterend", newP);
 
 //2.9
+
 const divs = document.querySelectorAll("div.fn-insert-here");
 for (const div of divs) {
   const insideP = document.createElement("p");
