28898787bafdb3526ba26f4a945b542f22397f22
Marina <marina.lsz522@gmail.com>
2024-09-27 17:50:25 +0200

    Modificado ejercicio3

--- ejercicio3/script.js
+++ ejercicio3/script.js
@@ -1,4 +1,5 @@
 //3.1
+
 const countries = ["Japón", "Nicaragua", "Suiza", "Australia", "Venezuela"];
 const ul = document.createElement("ul");
 for (const country of countries) {
@@ -9,10 +10,12 @@ for (const country of countries) {
 document.body.appendChild(ul);
 
 //3.2
+
 const p = document.querySelector(".fn-remove-me");
 p.remove();
 
 //3.3
+
 const cars = ["Mazda 6", "Ford fiesta", "Audi A4", "Toyota corola"];
 const carsContainer = document.querySelector('[data-function="printHere"]');
 const newUl = document.createElement("ul");
@@ -24,6 +27,7 @@ for (const car of cars) {
 carsContainer.appendChild(newUl);
 
 //3.4
+
 const fotos = [
   { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=1" },
   { title: "Random title", imgUrl: "https://picsum.photos/300/200?random=2" },
@@ -41,23 +45,35 @@ for (const foto of fotos) {
   img.alt = foto.title;
   div.appendChild(h4);
   div.appendChild(img);
-  div.classList.add("otra");
+  div.classList.add("imágenes");
   document.body.appendChild(div);
 }
 
 //3.5
+
 const button = document.createElement("button");
-button.textContent = "Eliminar último elemento";
-const divisor = document.querySelectorAll(".otra")
-console.log(divisor);
-
-button.addEventListener('click', () => {
-    const lastDiv = divisor.lastElementChild;
-    if (lastDiv) {
-        divisor.removeChild(lastDiv);
-    }
-});
-
+button.textContent = "Eliminar último div";
 document.body.appendChild(button);
 
+
+button.addEventListener("click", () => {
+  const divs = document.querySelectorAll(".imágenes");
+  if (divs.length > 0) {
+    for (let i = 0; i < divs.length; i++) {
+      divs[divs.length - 1].remove();
+    }
+  }
+});
+
 //3.6
+
+const divisores = document.querySelectorAll(".imágenes");
+for (let i = 0; i < divisores.length; i++) {
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Eliminar divisor";
+  divisores[i].appendChild(removeBtn);
+
+  removeBtn.addEventListener("click", () => {
+    divisores[i].remove();
+  });
+}
